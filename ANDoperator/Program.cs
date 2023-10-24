32d3acb19f26bd3a416c7e6322bebdb6e2282e80
Anastassia Brd. <anastassia.berdnikova@gmail.com>
2023-10-24 20:04:27 +0300

    add week 5 code

---
+++ ANDoperator/Program.cs
@@ -0,0 +1,24 @@
+﻿//Matk >=90; Biology >=90; Chemistry >= 90;
+
+int math, biology, chemistry;
+
+Console.WriteLine("Enter you math result");
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter you biology result");
+biology = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your chemistry result");
+chemistry = Int32.Parse(Console.ReadLine());
+
+if (math>=90 && biology>=90 && chemistry>=90)
+{
+    Console.WriteLine("Welcome to university!");
+}
+else
+{
+    Console.WriteLine("Try next year!");
+
+}
+
+Console.WriteLine("Have a nice day");
---
+++ Week5LogicalOperators/Program.cs
@@ -0,0 +1,13 @@
+﻿//and - &&
+
+//true && true --> true
+//true && false --> false
+//false && true --> false
+//false && false --> false
+
+//OR - || (pipes)
+
+//true || true --> true
+//true || false --> true 
+//false || true --> true
+//false || false --> false
